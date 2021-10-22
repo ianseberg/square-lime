1636247af8be75d459047e98e839ae6d3309bf9d
Stackbit <projects@stackbit.com>
2021-10-22 14:59:32 +0000

    initial commit by Stackbit

---
+++ gatsby-config.js
@@ -0,0 +1,75 @@
+require(`dotenv`).config()
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    siteTitleAlt: `Cemût - Piccola Osteria Furlana`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-jodie`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          { name: `Dove siamo`, slug: `/projects` },
+          { name: `Collab`, slug: `/art` },
+          { name: `Chi siamo`, slug: `/about` },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.gstatic.com`],
+        interval: 300,
+        timeout: 30000,
+        // If you plan on changing the font you'll also need to adjust the Theme UI config to edit the CSS
+        // See: https://github.com/LekoArts/gatsby-themes/tree/master/examples/jodie#changing-your-fonts
+        web: [
+          {
+            name: `Work Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Work+Sans:wght@400..700&display=swap`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Cemût - Piccola Osteria Furlana`,
+        short_name: `Cemût`,
+        description: `La tipica osteria friulana. A Trieste. Il meglio dell'enogastronomia regionale.`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#b75e09`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
